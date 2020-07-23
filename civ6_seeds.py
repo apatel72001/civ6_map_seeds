12d657065c98b2fc376ed07eb04d6b6d74c57488
Amit Patel <amit@pop-os.localdomain>
2020-07-23 16:12:50 -0700

    Clean up text messages and formatting

--- civ6_seeds.py
+++ civ6_seeds.py
@@ -1,7 +1,7 @@
 # Open save game and search for the 5-byte sequence '93 e5 23 06 03'
 
-with open("MENELIK.Civ6Save", 'rb') as civ6_sav:
-# with open("LADY SIX SKY.Civ6Save", 'rb') as civ6_sav:
+# with open("MENELIK.Civ6Save", 'rb') as civ6_sav:
+with open("LADY SIX SKY.Civ6Save", 'rb') as civ6_sav:
     data = civ6_sav.read()
     file_position = data.find(b'\x93\xe5\x23\x06\x03')
     
@@ -9,16 +9,16 @@ with open("MENELIK.Civ6Save", 'rb') as civ6_sav:
                 (data[file_position-2]*256*256) +
                 (data[file_position-3]*256) +
                 (data[file_position-4]))
-    print("Raw Seed: ", raw_seed)
+    print("Raw Seed:", raw_seed)
     
     max_positive_seed = 2**31 - 1
-    print("Max Positive Seed: ", max_positive_seed)
+    print("Max Positive Seed:", max_positive_seed)
 
     if raw_seed > max_positive_seed:
         raw_seed -= 2**32
-        print("Modified Raw Seed: ", raw_seed)
+        print("Negative Raw Seed:", raw_seed)
 
 game_random_seed = raw_seed
 map_random_seed = raw_seed + 1
 
-print("Game Random Seed:  ", game_random_seed, "\nMap Random Seed:   ", map_random_seed)
+print("Game Random Seed: ", game_random_seed, "\nMap Random Seed:  ", map_random_seed)
