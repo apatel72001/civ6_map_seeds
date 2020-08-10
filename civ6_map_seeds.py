b3d26c49b14f67c4d50066d6d6ff36658118ffb5
Amit Patel <apatel72001@outlook.com>
2020-08-10 11:26:37 -0700

    new save file

--- civ6_map_seeds.py
+++ civ6_map_seeds.py
@@ -11,7 +11,8 @@
 # save_game = "PETER - Torres del Paine"
 # save_game = "PETER-Diety-Yosemite"
 # save_game = "PETER 20 3240 BC"
-save_game = "FREDERICK - Roraima - Domination"
+# save_game = "FREDERICK - Roraima - Domination"
+save_game = "MENELIK II 24 3080 BC"
 
 print("Save Game File:", save_game)
 
